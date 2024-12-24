548774e1c7f66fc5255b22b3ad54e4d140c7a2cc
Neverrie <korobov.ka@students.dvfu.ru>
2024-12-24 20:34:21 +1000

    Initial Commit

---
+++ MyProjectTests/MyProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectTests.swift
+//  MyProjectTests
+//
+//  Created by Константин Коробов on 24.12.2024.
+//
+
+import Testing
+@testable import MyProject
+
+struct MyProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
